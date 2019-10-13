18357e2d10eb0c16c24d7c8f18502b915254c86d
Peery <peery.coding@gmail.com>
2019-10-13 18:22:57 +0200

    Fixed loadConfigJson
    fixed unreachable code and unused parameter in loadConfigJson()

--- config_load.py
+++ config_load.py
@@ -1,19 +1,22 @@
-import json 
+import json
+
 
 def saveConfig(configs):
-	with open(str(input('Config File Name: ')), 'w') as outfile:  
-		json.dump(configs, outfile, indent=4)
-		#outfile.close()
+    with open(str(input('Config File Name: ')), 'w') as outfile:
+        json.dump(configs, outfile, indent=4)
+    # outfile.close()
 
-def loadConfigJson(configs):
-	with open('mudpi.config') as loadedfile:
-		configs = json.load(loadedfile)
-		return configs
-		loadedfile.close()
+
+def loadConfigJson():
+    with open('mudpi.config') as loadedfile:
+        configs = json.load(loadedfile)
+        loadedfile.close()
+        return configs
+
 
 def loadConfig(configs):
-	with open("config.mud") as configfile:
-		for line in configfile:
-			name, var = line.partition("=")[::2]
-			configs[name.strip()] = (var)
-		configfile.close()
+    with open("config.mud") as configfile:
+        for line in configfile:
+            name, var = line.partition("=")[::2]
+            configs[name.strip()] = (var)
+        configfile.close()
