3395fb2d472d57118385d354c640ef85a9abfe4a
Eric Davisson <relaeh776@gmail.com>
2019-10-22 16:54:03 -0500

    v0.8.4 Adruino Worker Refactors & New Controls
    Updating version to reflect last batch of feature updates and refactors.

--- mudpi.py
+++ mudpi.py
@@ -61,7 +61,7 @@ print('|_|  |_|\__,_|\__,_|_|   |_| ')
 print('_________________________________________________')
 print('')
 print('Eric Davisson @theDavisson')
-print('Version: ', CONFIGS.get('version', '0.8.3'))
+print('Version: ', CONFIGS.get('version', '0.8.4'))
 print('\033[0;0m')
 
 if CONFIGS['debug'] is True:
