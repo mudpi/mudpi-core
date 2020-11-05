58fd4faebf7ea0d5fdf04fdcb69a5947204fe3f6
Jordan ERNST <pro.ernst@gmail.com>
2020-11-05 23:22:08 +0100

    PEP8 changes in linux/i2c/sensor.py

--- sensors/linux/i2c/sensor.py
+++ sensors/linux/i2c/sensor.py
@@ -5,7 +5,8 @@ import board
 from busio import I2C
 import RPi.GPIO as GPIO
 
-#PIN MODE : OUT | IN
+
+# PIN MODE : OUT | IN
 
 class Sensor():
 
@@ -31,21 +32,21 @@ class Sensor():
         return
 
     def init_sensor(self):
-        #Initialize the sensor here (i.e. set pin mode, get addresses, etc)
-        #GPIO.setmode(GPIO.BCM)
-        #GPIO.setup(pin, GPIO.IN)
+        """Initialize the sensor here (i.e. set pin mode, get addresses, etc)"""
+        # GPIO.setmode(GPIO.BCM)
+        # GPIO.setup(pin, GPIO.IN)
         pass
 
     def read(self):
-        #Read the sensor(s), parse the data and store it in redis if redis is configured
-        #GPIO.input(pin)
+        """Read the sensor(s), parse the data and store it in redis if redis is configured"""
+        # GPIO.input(pin)
         pass
 
     def readRaw(self):
-        #Read the sensor(s) but return the raw data, useful for debugging
+        # Read the sensor(s) but return the raw data, useful for debugging
         pass
 
     def readPin(self):
-        #Read the pin from the ardiuno. Can be analog or digital based on "analog_pin_mode"
+        # Read the pin from the ardiuno. Can be analog or digital based on "analog_pin_mode"
         data = self.gpio.input(self.pin)
         return data
