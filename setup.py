cd4c6adea1f20b818511170f64287b0d028e5ed7
Eric Davisson <eric@mudpi.app>
2021-03-15 11:45:22 -0500

    Updated install

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     author="Eric Davisson",
     author_email="eric@mudpi.app",
     url="https://mudpi.app",
+    include_package_data=True,
+    package_data={'': ['*.json', '*.yml', '*.config', '*.config.example', '*.sh']},
     packages=find_packages(exclude=['tools', 'tests', 'scripts', 'debug']),
     entry_points={
         'console_scripts': [
